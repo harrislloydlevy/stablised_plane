18321e8e3b3d6bc62a3d0905a0918112f94f084d
Harris Lloyd-Levy <harris.lloydlevy@gmail.com>
2025-10-16 22:15:55 +1100

    Full fledge re-work to actually read from GY85, integrate degress

--- stablised_plane.ino
+++ stablised_plane.ino
@@ -1,28 +1,43 @@
 // Include the PPM library
 #include "ppm.h"
 #include "Servo.h"
-#include "GY_85.h"
+#include "ITG3200.h"
 #include "ArduPID.h"
 
 #define dbg(var) Serial.print(#var); Serial.print(":"); Serial.print(var); Serial.print(" ")
 
+// Rotation around the forward axis is called roll, and is changed by the aelirons
 ArduPID aerController;
 Servo   aerServo;
-double  aerSetpoint;
-double  aerInput;
+double  targetRoll;
+double  currentRoll;
 double  aerOutput;
 double  aerP = 1;
 double  aerI = 0;
 double  aerD = 0;
 
+// Rotation around the east-west axis is called pitch and is changed by the elevator
 ArduPID eleController;
 Servo   eleServo;
-double  eleSetpoint;
-double  eleInput;
+double  targetPitch;
+double  currentPitch;
 double  eleOutput;
 double  eleP = 1;
 double  eleI = 0;
 double  eleD = 0;
+
+// Rotation around the up-down axis is called yaw and is changed by the rudder
+ArduPID rudController;
+Servo   rudServo;
+double  targetYaw;
+double  currentYaw;
+double  rudOutput;
+double  rudP = 1;
+double  rudI = 0;
+double  rudD = 0;
+
+// Throttle is simply throttle :).
+double throttle;
 
 // PPM channel layout (update for your situation)
 #define THROTTLE 3
@@ -35,103 +50,113 @@ double  eleD = 0;
 #define POT 8           // trim-pot on the (front left edge trim)
 
 // Loop interval time
-const long interval = 50;
-unsigned long previousMillis = 0;
+const long ppmInterval = 100;
+const long gyroInterval = 100;
+unsigned long previousGyroMillis = 0;
+unsigned long previousPpmMillis = 0;
 
-
-
-GY_85 GY85;
+ITG3200 gyro;
 
 void setup() {
   // Start the serial port to display data
-  Serial.begin(115200);
+  Serial.begin(9600);
 
-  aerController.begin(&aerInput, &aerOutput, &aerSetpoint, aerP, aerI, aerD);
+  aerController.begin(&currentRoll, &aerOutput, &targetRoll, aerP, aerI, aerD);
   aerServo.attach(10);
 
-  eleController.begin(&eleInput, &eleOutput, &eleSetpoint, eleP, eleI, eleD);
+  eleController.begin(&currentPitch, &eleOutput, &targetPitch, eleP, eleI, eleD);
   eleServo.attach(9);
 
-  // Start the PPM function on PIN A0
-  ppm.begin(4, false);
-  GY85.init();
+  aerController.begin(&currentYaw, &aerOutput, &targetYaw, aerP, aerI, aerD);
+  aerServo.attach(8);
+
+  // Start the PPM receiver on PIN A0
+  ppm.begin(A0, false);
+  
+  // Gyro is on generic i2c so no explictit pin needed.
+  gyro.init();
+  gyro.zeroCalibrate(200,10);//sample 200 times to calibrate and it will take 200*10ms
+
+  // Set initial zero of angle of plane
+  currentYaw = 0;
+  currentRoll = 0;
+  currentPitch = 0;
+    // Set initial zero of angle of plane
+  targetYaw = 0;
+  targetRoll = 0;
+  targetPitch = 0;
 }
 
 void loop() {
+  float gx, gy, gz; // Angular momentum in deg/sec in each axis
+
   // Interval at which the PPM is updated
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
+  // If we have passed the cycle for reading the PPM, then read in a new channel
+  // of input
+  if ((currentMillis - previousPpmMillis) >= ppmInterval) {
+    previousPpmMillis = currentMillis;
 
     // Acquiring all the channels values
-    aerSetpoint = map(ppm.read_channel(ROLL), 1000, 2000, 0, 180);
-    eleSetpoint = map(ppm.read_channel(PITCH), 1000, 2000, 0, 180);  
+    //aerSetpoint = map(ppm.read_channel(ROLL), 1000, 2000, 0, 180);
+    //eleSetpoint = map(ppm.read_channel(PITCH), 1000, 2000, 0, 180);
+    targetRoll = ppm.read_channel(ROLL);
+    targetPitch = ppm.read_channel(PITCH);
+    targetYaw = ppm.read_channel(YAW);
+    throttle = ppm.read_channel(THROTTLE);
 
-    
+    dbg(throttle);
+    dbg(targetRoll);
+    dbg(targetPitch);
+    dbg(targetYaw);
   }
 
-  int* compassReadings = GY85.readFromCompass();
-  int cx = GY85.compass_x(compassReadings);
-  int cy = GY85.compass_y(compassReadings);
-  int cz = GY85.compass_z(compassReadings);
-
-  dbg(cx);
-  dbg(cy);
-  dbg(cz);
+  // If we have passed the cycle for reading the velocity, then read in and reintegrate
+  // our target angle
+  if ((currentMillis - previousGyroMillis) >= gyroInterval) {
+    double ratio = (((double)currentMillis/1000 - (double)previousGyroMillis/1000));
 
 
-  float* gyroReadings = GY85.readGyro();
-  float gx = GY85.gyro_x(gyroReadings);
-  float gy = GY85.gyro_y(gyroReadings);
-  float gz = GY85.gyro_z(gyroReadings);
-  float gt = GY85.temp(gyroReadings);
+    gyro.getAngularVelocity(&gx,&gy, &gz);
+    currentYaw += gx * ratio;
+    currentRoll += gy * ratio;
+    currentPitch += gz * ratio;
 
-  dbg(gx);
-  dbg(gy);
-  dbg(gz);
+    dbg(currentMillis);
+    dbg(previousGyroMillis);
+    dbg(ratio);
+    dbg(currentYaw);
+    dbg(currentRoll);
+    dbg(currentPitch);
+    dbg(gx);
+    dbg(gy);
+    dbg(gz);
+
+    previousGyroMillis = currentMillis;
+  }
+
 
 
   Serial.println();
-  aerInput = map(cx, -1000, 1000, -90, 90);
-  eleInput = map(cy, -1000, 1000, -90, 90);
+  //aerInput = map(gx, -1000, 1000, -90, 90);
+  //eleInput = map(gz, -1000, 1000, -90, 90);
   
 
 
-  /* Serial.print(" CurrentRoll:");
-  Serial.print(cx);
-  Serial.print(" TargetRoll:");
-  Serial.print(roll);
-  Serial.print(" AerilonAngle:");
-  Serial.print(aerilonAngle);
-  Serial.println();
-  */
-
-  /*
-  Serial.print(" cy:");
-  Serial.print(cy);
-  Serial.print(" cz:");
-  Serial.print(cz);
-
-  Serial.print("\t  gyro");
-  Serial.print(" gx:");
-  Serial.print(gx);*/
   aerController.compute();
   eleController.compute();
   //aerController.debug(&Serial, "aerController", PRINT_INPUT |     // Can include or comment out any of these terms to print
     //                                                      PRINT_OUTPUT |  // in the Serial plotter
       //                                                    PRINT_SETPOINT | PRINT_BIAS | PRINT_P | PRINT_I | PRINT_D);
-  
-
-  // Make delay between readings
-  delay(100);
-
 
   aerServo.write(aerOutput);
   eleServo.write(eleOutput);
-  
-
 }
+
+
+
+
 // Returns what angle (from 0 to 180) the aeriolon should be set at based on the detected angle of the plane
 // based on the target angle. 90 is neutral, 0 is full title left, 180 is full tilt right
 //int aerOutput(int currentRoll, int targetRoll) {
